4a9a18e5e1913fc58cb3f9c817d94eeb1e02bca2
bebek <155866565+bebekSambal@users.noreply.github.com>
2024-02-14 00:37:02 +0700

    bentuk fungsi biasa

--- app-1.js
+++ app-1.js
@@ -1,3 +1,9 @@
 const hex = (r,g,b) => {
     return '#' + ((1 << 24)+(r << 16)+(g << 8)+b).toString(16).slice(1);
 };
+
+// bentuk fungsi biasa
+
+// function hexLain(r,g,b){
+//    return '#' + ((1 << 24)+(r << 16)+(g << 8)+b).toString(16).slice(1);
+// };
