1a5f2b3631541486dc54b369ddc9ca51a50d18bf
bebek <155866565+bebekSambal@users.noreply.github.com>
2024-02-14 15:35:49 +0700

    Update app-1.js
    latihan factory function- OOP

--- app-1.js
+++ app-1.js
@@ -1,9 +1,56 @@
-const hex = (r,g,b) => {
-    return '#' + ((1 << 24)+(r << 16)+(g << 8)+b).toString(16).slice(1);
-};
+// const hex = (r,g,b) => {
+//     return '#' + ((1 << 24)+(r << 16)+(g << 8)+b).toString(16).slice(1);
+// };
 
 // bentuk fungsi biasa
 
 // function hexLain(r,g,b){
 //    return '#' + ((1 << 24)+(r << 16)+(g << 8)+b).toString(16).slice(1);
 // };
+
+// ------------------------------------------------
+
+// const rgb = (r,g,b) => {
+//     return `rgb(${r}, ${g}, ${b})`;
+// };
+
+function konversiKolor (r,g,b) {
+    const color = {};
+   
+    color.r = r;
+    color.g = g;
+    color.b = b;
+
+    color.rbg = function () {
+        const { r, g, b } = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+
+    color.hex = function () {
+        const { r, g, b } = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    };
+
+    return color;
+};
+
+
+
+
+
+
+
+// ----- factory function dengan contoh dari chatgpt
+
+function buatPerson(nama,umur){
+    return {
+        nama : nama,
+        umur : umur,
+        sayHello : function() {
+            return `hello ${nama} berumur ${umur} tahun` ;
+        }
+    };
+};
+
+let personBaru = buatPerson('moko', 77);
+let anu = personBaru.sayHello();
